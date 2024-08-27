472d4053ef95f60f4d5ee55cf882bad8a0514e71
Marcos Camargo <132371327+marcaocamargo@users.noreply.github.com>
2024-08-27 12:09:52 -0300

    Update and rename file2_name.py to calculate.py

---
+++ package_name/calculate.py
@@ -0,0 +1,22 @@
+def calculator():
+    print("Selecione a operação:")
+    print("1. Adição")
+    print("2. Subtração")
+    print("3. Multiplicação")
+    print("4. Divisão")
+
+    choice = input("Digite a escolha (1/2/3/4): ")
+
+    num1 = float(input("Digite o primeiro número: "))
+    num2 = float(input("Digite o segundo número: "))
+
+    if choice == '1':
+        print(f"{num1} + {num2} = {add(num1, num2)}")
+    elif choice == '2':
+        print(f"{num1} - {num2} = {subtract(num1, num2)}")
+    elif choice == '3':
+        print(f"{num1} * {num2} = {multiply(num1, num2)}")
+    elif choice == '4':
+        print(f"{num1} / {num2} = {divide(num1, num2)}")
+    else:
+        print("Escolha inválida")
